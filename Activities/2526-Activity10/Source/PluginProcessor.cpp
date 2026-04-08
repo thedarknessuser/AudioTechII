c224ebd1439ff0bbc730fbfbc05df68066da779a
Bennett, Jennefer <2jpbennett@gmail.com>
2026-04-07 20:08:47 -0400

    Added Activity 10

--- Activities/2526-Activity10/Source/PluginProcessor.cpp
+++ Activities/2526-Activity10/Source/PluginProcessor.cpp
@@ -91,10 +91,20 @@ void _2526Activity10AudioProcessor::changeProgramName (int index, const juce::St
 }
 
 //==============================================================================
-void _2526Activity10AudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void _2526Activity10AudioProcessor::prepareToPlay (double sampleRate, int numSamplesPerBlock)
 {
     // You need to initialize your variables here!
-    
+    samplingRate = sampleRate;
+    samplesPerBlock = numSamplesPerBlock;
+
+    freq = 440;
+    amp = 1;
+    phase = 0;
+
+    // envelop length in samples
+    int envSamples = samplingRate * int(envSec);
+
+    int envTracker = 0;
 }
 
 void _2526Activity10AudioProcessor::releaseResources()
@@ -152,6 +162,33 @@ void _2526Activity10AudioProcessor::genSineWave(juce::AudioBuffer<float>& buffer
 {
     // Fill the buffer (in place) with a sinusoid
     // your code goes here!
+
+    // for loop for channels, for loop for phase
+    // increment record keeping varaibles
+
+    //float phaseStart = phase;
+    //for (int channel = 0; channel < buffer.getNumChannels(); ++channel) {
+    //    phase = phaseStart;
+    //    auto* channelData = buffer.getWritePointer(channel);
+    //    for (int i = 0; i < blockSize; ++i) {
+    //        channelData[i] = amp * sinf(phase);
+
+    //        phase += juce::MathConstants<float>::twoPi * freq;
+    //    }
+    //}
+    float phaseStart = phase;
+    for (int channel = 0; channel < buffer.getNumChannels(); ++channel) {
+        auto* channelData = buffer.getWritePointer(channel);
+        phase = phaseStart;
+        for (int i = 0; i < samplesPerBlock; ++i) {
+            channelData[i] = amp * sinf(phase);
+            phase += juce::MathConstants<float>::twoPi * freq / samplingRate;
+
+            if (phase >= juce::MathConstants<float>::twoPi) {
+                phase -= juce::MathConstants<float>::twoPi;
+            }
+        }
+    }
     
 }
 
@@ -161,6 +198,32 @@ void _2526Activity10AudioProcessor::applyEnvRamp(juce::AudioBuffer<float>& buffe
     // Apply an amplitude envelope to the buffer (in place)
     // Multiply each sample by an envelope value (0 → 1 → 0)
     // your code goes here!
+
+    int envStart = envTracker;
+    float envVal;
+    float halfEnvLen = float(envSamples) / 2;
+
+    for (int channel = 0; channel < buffer.getNumChannels(); ++channel) {
+        auto* channelData = buffer.getWritePointer(channel);
+        envTracker = envStart;
+        for (int i = 0; i < samplesPerBlock; ++i) {
+            if (envTracker < halfEnvLen) {
+                envVal = envTracker / halfEnvLen;
+            }
+            else {
+                envVal = 1.0f - (envTracker - halfEnvLen) / halfEnvLen;
+            }
+
+            channelData[i] *= envVal;
+
+            envTracker++;
+
+            if (envTracker > envSamples) {
+                envTracker = 0;
+            }
+        }
+
+    }
 }
 
 //==============================================================================
